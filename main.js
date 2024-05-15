2b68bc3e3d83de47f6a65f1a0f7c8aba4a87dc03
Rafael Souto <rsowtto@gmail.com>
2024-05-15 00:51:43 -0300

    Projeto funcionando

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const url = "https://api.escuelajs.co/api/v1/products";
+
 const URL2 = "https://fakestoreapi.com/products";
 const secaoLivros = document.querySelector("#livros");
+const BotoesDeFiltro = document.querySelectorAll(".nav>li>button");
+const inputDePesquisa = document.querySelector(".banner__pesquisa");
+let produtos = [];
 async function buscar()
 {
     try 
     {
         const requisicao = await fetch(URL2);
-        const produtos = await requisicao.json();
+        produtos = await requisicao.json();
         
         exibirProdutos(produtos);
     }
@@ -18,8 +21,8 @@ async function buscar()
 
 function exibirProdutos(produtos)
 {   
-
   let maximoDeCaracteres = 300;
+  secaoLivros.innerHTML = "";
   produtos.forEach(produto =>
   {   
       secaoLivros.innerHTML += 
@@ -40,6 +43,59 @@ function exibirProdutos(produtos)
       `;
   });
 }
-const obj = {imagem: ["1","2"], nome: "Rafa"}
-if(obj.hasOwnProperty("nome")) console.log("tem a propriedade:", obj.nome);
+
+function filtrar()
+{
+  const id = this.id;
+  const categoria =  this.value;
+  filtrarPorCategoria(categoria);
+  categoria == "disponivel" ? 
+    filtrarPorQuantidade() : id == "btnOrdenarPorPreco" ? 
+      ordenarPorPreco() : id == "btnTodos" ? 
+        exibirProdutos(produtos) : filtrarPorCategoria(categoria);
+}
+
+function filtrarPorCategoria(categoria)
+{
+  const produtosDaCategoria = produtos.filter(produto => produto.category == categoria);
+  exibirProdutos(produtosDaCategoria);
+}
+
+function filtrarPorQuantidade()
+{
+    const disponiveis = produtos.filter(produto => produto.count > 0);
+    console.log(disponiveis)
+    exibirProdutos(disponiveis);
+}
+
+function ordenarPorPreco()
+{
+  
+  const produtosOrdenados = [...produtos].sort((a, b) => a.price - b.price);
+  exibirProdutos(produtosOrdenados);
+}
+
+function filtrarNome(evento)
+{
+  const valorBarraPesquisa = inputDePesquisa.value.toLowerCase();
+  const produtosParaFiltrar = document.querySelectorAll(".livro");
+  produtosParaFiltrar.forEach(produto => 
+    {
+      const tituloDoProduto = produto.querySelector(".livro__titulo").textContent.toLowerCase();
+      console.log(tituloDoProduto)
+      produto.style.display = 
+        tituloDoProduto.includes(valorBarraPesquisa) || valorBarraPesquisa == "" ?
+          "flex" : "none";
+    }
+  )
+}
+
 buscar();
+
+inputDePesquisa.addEventListener("input", filtrarNome);
+
+BotoesDeFiltro.forEach(botao =>
+  {
+    botao.addEventListener("click", filtrar)
+  }
+);
