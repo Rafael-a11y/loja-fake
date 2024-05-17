246c85774b6ca5e5c7c5aa9f54a919bb2353cb6d
Rafael Souto <rsowtto@gmail.com>
2024-05-17 00:48:29 -0300

    Projeto temporariamente finalizado

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const URL2 = "https://fakestoreapi.com/products";
 const secaoLivros = document.querySelector("#livros");
 const BotoesDeFiltro = document.querySelectorAll(".nav>li>button");
 const inputDePesquisa = document.querySelector(".banner__pesquisa");
+const valorDeTodosOsProdutos = document.querySelector("#valor");
 let produtos = [];
 async function buscar()
 {
@@ -37,11 +38,12 @@ function exibirProdutos(produtos)
         produto.description.substring(0, maximoDeCaracteres + 1) + "..." : produto.description}</p>
       <p class="livro__preco" id="preco">${produto.price}</p>
       <span class="tag">${produto.category}</span>
-      <button class="livro__botao">Adicionar ao carrinho de compras</button>
+      <button class="livro__botao-adicionar-ao-carrinho">Adicionar ao carrinho de compras</button>
       
     </div>
       `;
   });
+  somaDosValores(produtos);
 }
 
 function filtrar()
@@ -64,7 +66,6 @@ function filtrarPorCategoria(categoria)
 function filtrarPorQuantidade()
 {
     const disponiveis = produtos.filter(produto => produto.count > 0);
-    console.log(disponiveis)
     exibirProdutos(disponiveis);
 }
 
@@ -78,16 +79,32 @@ function ordenarPorPreco()
 function filtrarNome(evento)
 {
   const valorBarraPesquisa = inputDePesquisa.value.toLowerCase();
-  const produtosParaFiltrar = document.querySelectorAll(".livro");
+  const produtosParaFiltrar = Array.from(document.querySelectorAll(".livro"));
+  let paraSomar = [];
   produtosParaFiltrar.forEach(produto => 
     {
       const tituloDoProduto = produto.querySelector(".livro__titulo").textContent.toLowerCase();
-      console.log(tituloDoProduto)
-      produto.style.display = 
-        tituloDoProduto.includes(valorBarraPesquisa) || valorBarraPesquisa == "" ?
-          "flex" : "none";
+      produto.style.display = tituloDoProduto.includes(valorBarraPesquisa) || valorBarraPesquisa == "" ?
+       "flex" : "none";
+       if(produto.style.display == "flex")
+        {
+          const preco = 
+          {
+            price: Number(produto.querySelector(".livro__preco").textContent)
+          };
+          paraSomar.push(preco);
+        }
     }
-  )
+  );
+  somaDosValores(paraSomar);
+}
+
+function somaDosValores(produtos)
+{
+  console.log(produtos)
+  const acumulo = produtos.reduce((acumulador, produto) => {return acumulador + produto.price}, 0)
+  .toFixed(2).replace(".", ",");
+  valorDeTodosOsProdutos.innerHTML = acumulo;
 }
 
 buscar();
